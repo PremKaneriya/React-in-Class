379f0b41da21b7b7182d23b5e3237dad87e5bf19
Prem Kaneriya <premkaneriyappp@gmail.com>
2024-02-28 13:16:48 +0530

    Done Day 2

--- Lecture_1/Lecture_1/src/App.jsx
+++ Lecture_1/Lecture_1/src/App.jsx
@@ -1,10 +1,19 @@
+import ClassBox from "./components/ClassBox/ClassBox";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import ClassBox_2 from "./components/ClassBox_2/ClassBox_2";
 
 const App = () => {
   return (
-    <div>
-      
+    <>
+    <Header />
+    <div className="flex items-center justify-center">
+    <ClassBox />
+    <ClassBox_2 />
     </div>
-  )
-}
+    <Footer />    
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ Lecture_1/Lecture_1/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <>
+      <h1 className="text-3xl py-5 hover:text-green-500 transition-all bg-blue-950 font-bold font-mono text-blue-500 text-center absolute bottom-0 w-full">Footer</h1>
+    </>
+  )
+}
+
+export default Footer
