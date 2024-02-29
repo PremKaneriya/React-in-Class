0f154998d42541d55411a7054b937892e8680377
Prem Kaneriya <premkaneriyappp@gmail.com>
2024-02-29 10:42:26 +0530

    Done

--- Lecture_1/Lecture_1/src/App.jsx
+++ Lecture_1/Lecture_1/src/App.jsx
@@ -1,8 +1,3 @@
-// import ClassBox from "./components/ClassBox/ClassBox";
-// import Footer from "./components/Footer/Footer";
-// import Header from "./components/Header/Header";
-// import ClassBox_2 from "./components/ClassBox_2/ClassBox_2";
-
 import City from "./user/components/City";
 import CityFunc from "./user/components/CityFunc";
 import Country from "./user/components/Country"
@@ -11,12 +6,6 @@ import CountryFunc from "./user/components/CountryFunc";
 const App = () => {
   return (
     <>
-      {/* <Header />
-      <div className="flex items-center justify-center">
-        <ClassBox />
-        <ClassBox_2 />
-      </div>
-      <Footer /> */}
       <Country />
       <City />
       <CountryFunc />
