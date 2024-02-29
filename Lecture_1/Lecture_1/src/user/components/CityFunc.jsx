1860bd5c7aa9dc136a8751b6735177c474962349
Prem Kaneriya <premkaneriyappp@gmail.com>
2024-02-29 10:40:56 +0530

    Done Homework

--- Lecture_1/Lecture_1/src/user/components/CityFunc.jsx
+++ Lecture_1/Lecture_1/src/user/components/CityFunc.jsx
@@ -1,12 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { useState } from "react";
 
-function CityFunc(props) {
+function CityFunc() {
+  const [change, setchange] = useState({ city: "Bengaluru" , timezone : "GMT+5:30" });
+
+  const changeCity = () => {
+    setchange({ city: "Los Angles" , timezone : "GMT-3:30" })
+  }
+
   return (
     <>
       <h1 className="text-3xl px-5 py-5 text-slate-900">
-        This is City function based componenets
+        city : {change.city}
       </h1>
+      <h2 className="text-3xl px-5 py-5 text-slate-900">
+        timezone : {change.timezone}
+      </h2>
+      <button onClick={changeCity} className="bg-slate-500 py-1 px-1 ml-4 mb-5 rounded-lg text-white">Change Info</button>
     </>
   );
 }
--- Lecture_1/Lecture_1/src/user/components/CountryFunc.jsx
+++ Lecture_1/Lecture_1/src/user/components/CountryFunc.jsx
@@ -1,11 +1,21 @@
-/* eslint-disable no-unused-vars */
+
+// eslint-disable-next-line no-unused-vars
+import { useState } from "react";
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
-function CountryFunc(props) {
+function CountryFunc() {
+    const [change, setchange] = useState({name:"india", timezone : "GMT+5:30"})
+
+    const changeCountry = () => {
+        setchange({name:"USA", timezone:"GMT+5:00"})
+    }
+    
     return (
         <>
-            <h1 className="text-3xl mt-5 px-5 py-5 text-slate-500">This is country function based componenets</h1>
+            <h1 className="text-3xl mt-5 px-5 py-5 text-slate-500">This is country : {change.name}</h1>
+            <h1 className="text-3xl mt-5 px-5 py-5 text-slate-500">Timezone: {change.timezone}</h1>
+            <button onClick={changeCountry} className="bg-slate-500 py-1 px-1 ml-4 mb-5 rounded-lg text-white">Change Info</button>
         </>
     );
 }
