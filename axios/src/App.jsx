406323e44adb87f1e391194bdf286dc589e46374
Prem Kaneriya <premkaneriyappp@gmail.com>
2024-03-28 11:47:26 +0530

    Date_28-3-24_Done_Homework

---
+++ axios/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://fakestoreapi.com/products')
+      
+      setData(response.data)
+      setLoading(false)
+    } catch (error) {
+      setError(error.message)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (loading) return <h1 className="text-8xl text-center mt-40 capitalize font-bold py-5">Loading...</h1>
+
+  if (error) return <h1>{error}</h1>
+
+  return (
+    <>
+      <h1 className="text-4xl text-center capitalize font-bold bg-blue-300 text-blue-800 py-5">
+        axios
+      </h1>
+      <div className="flex flex-wrap justify
+      -center">
+
+        {data.map((item) => (
+          <div key={item.id} className="bg-blue-200 m-5 p-5 rounded-lg shadow-lg">
+            <h1>{item.title}</h1>
+            <p>{item.price}</p>
+          </div>
+        ))}
+      </div>
+
+    </>
+  );
+}
+
+export default App;
